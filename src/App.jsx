d765fbe1aba4d96965386f75c9660567eb1edcdf
Rodz Andrie Amor <ramorstem2022@example.com>
2023-04-09 10:25:04 -0400

    Small styling changes

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ const Card = ({name, image, description, link, github, instagram}) => {
       <figure><img width={288} height={288} src={image} alt="Image" /></figure>
       <div className="card-body">
         <h2 className="card-title">{name}</h2>
-        <p>{description}</p>
+        <p className="mb-2">{description}</p>
         <div className="card-actions justify-center">
           <SocialIcon url={link} target="_blank" />
           {github ? <SocialIcon url={github} target="_blank" /> : <></>}
